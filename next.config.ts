d44c731175ab380029f72edb606fadc8b6541c96
Kartik Joshi <75471221+kartikj360@users.noreply.github.com>
2025-09-09 16:17:27 +0530

    Deployed

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  // For project pages (https://username.github.io/repo), also set:
-  // basePath: "/<your-repo-name>",
-  // assetPrefix: "/<your-repo-name>/",
+  basePath: "kartik-portfolio",
+  assetPrefix: "/kartik-portfolio/",
 };
 
 export default nextConfig;
