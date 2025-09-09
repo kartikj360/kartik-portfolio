0f78995a63ccdf078136f55dc85c6784515e2ef4
Kartik Joshi <75471221+kartikj360@users.noreply.github.com>
2025-09-09 16:21:03 +0530

    Deploy2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath: "kartik-portfolio",
+  basePath: "/kartik-portfolio",
   assetPrefix: "/kartik-portfolio/",
 };
 
